48d2e9a41905129928b58c6b161f3625721ace16
cyteon <129582290+Cyteon@users.noreply.github.com>
2024-12-20 11:57:44 +0100

    more minor changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -38,14 +38,20 @@
         <img src="/doubloon.png" class="inline-block" alt="Doubloon" height="72" width="72">
         <span class="ml-2 flex flex-col my-auto">
             <span class="text-6xl font-bold">Doubloons</span>
-            <span class="text-start text-sm ml-1 text-muted">(actual doubloon data)</span>
+            <span class="text-start text-sm ml-1 text-muted">(actual doubloon data, for opted-in people)</span>
         </span>
     </h1>
 
     <div class="p-3 shadow-md rounded-lg mx-auto mt-4 mb-8 mx-2 md:mx-8 h-full bg-surface">
-        <div class="flex flex-col md:flex-row">
-            <h2 class="text-4xl font-semibold">Leaderboard</h2>
-            <div class="ml-auto flex">
+        <div class="flex flex-col md:flex-row mb-4">
+            <div>
+                <h2 class="text-4xl font-semibold">Leaderboard</h2>
+                <p class="text-lg text-muted mt-2 md:mt-0">
+                    Opt-in/out at
+                    <a href="https://highseas.hackclub.com/signpost" class="text-blue">The Signpost</a>!
+                </p>
+            </div>
+            <div class="ml-auto flex h-fit">
                 <input 
                 type="text" 
                 class="text-lg w-full p-2 rounded-lg outline-none border border-border transition-all duration-300 bg-base" 
@@ -80,18 +86,16 @@
         {/if}
 
         {#each data as user, i}
-            {#if user != searched}
-                <div class={`flex p-2 ${i % 2 == 0 ? '' : 'bg-base'}`}>
-                    <p class="text-2xl my-auto font-semibold mr-6">#{i + 1}</p>
+            <div class={`flex p-2 ${i % 2 == 0 ? '' : 'bg-base'}`}>
+                <p class="text-2xl my-auto font-semibold mr-6">#{i + 1}</p>
 
-                    <img src={user.pfp} class="rounded-full" height="48" width="48" alt="profile" />
-                    <h1 class="text-2xl my-auto font-semibold ml-2 mr-10">@{user.username}</h1>
-                    <p class="ml-auto text-2xl my-auto font-semibold flex">
-                        <span class="my-auto mr-1">{user.doubloons}</span>
-                        <img src="/doubloon.png" class="inline-block" alt="Doubloon" height="24" width="24">
-                    </p>
-                </div>  
-            {/if}
+                <img src={user.pfp} class="rounded-full" height="48" width="48" alt="profile" />
+                <h1 class="text-2xl my-auto font-semibold ml-2 mr-10">@{user.username}</h1>
+                <p class="ml-auto text-2xl my-auto font-semibold flex">
+                    <span class="my-auto mr-1">{user.doubloons}</span>
+                    <img src="/doubloon.png" class="inline-block" alt="Doubloon" height="24" width="24">
+                </p>
+            </div>  
         {/each}
     </div>
 </div>
