31b314223793737b5cbd568d09cfcd823d5e7da9
cyteon <cyteon@hackclub.app>
2025-01-03 18:16:42 +0100

    redis

--- src/routes/api/v1/data/+server.ts
+++ src/routes/api/v1/data/+server.ts
@@ -1,4 +1,16 @@
 import fs from "fs";
+import redis from "redis";
+import { REDIS_URI } from "$env/static/private";
+
+const client = redis.createClient({ url: REDIS_URI });
+
+client.connect()
+    .then(() => {
+        console.log("Connected to Redis.");
+    })
+    .catch((e) => {
+        console.error("Failed to connect to Redis: ", e);
+    });
 
 async function fetchData() {
     const res = await fetch("https://airtable.com/v0.3/application/appTeNFYcUiYfGcR6/readForSharedPages?stringifiedObjectParams=%7B%22includeDataForPageId%22%3A%22pagblWwXt2nF2bRKu%22%2C%22shouldIncludeSchemaChecksum%22%3Atrue%2C%22expectedPageLayoutSchemaVersion%22%3A26%2C%22shouldPreloadQueries%22%3Atrue%2C%22shouldPreloadAllPossibleContainerElementQueries%22%3Atrue%2C%22urlSearch%22%3A%22iyC4S%253Asort%3DeyJwZWxyRDBBVmZJU282c0RGbCI6eyJjb2x1bW5JZCI6ImZsZDRRb2Z2NUxNR3RsRmxwIiwiYXNjZW5kaW5nIjpmYWxzZX19%22%2C%22includeDataForExpandedRowPageFromQueryContainer%22%3Atrue%2C%22includeDataForAllReferencedExpandedRowPagesInLayout%22%3Atrue%2C%22navigationMode%22%3A%22view%22%7D&requestId=reqF7W1PBvHXwBOfz&accessPolicy=%7B%22allowedActions%22%3A%5B%7B%22modelClassName%22%3A%22page%22%2C%22modelIdSelector%22%3A%22pagblWwXt2nF2bRKu%22%2C%22action%22%3A%22read%22%7D%2C%7B%22modelClassName%22%3A%22application%22%2C%22modelIdSelector%22%3A%22appTeNFYcUiYfGcR6%22%2C%22action%22%3A%22readForSharedPages%22%7D%2C%7B%22modelClassName%22%3A%22application%22%2C%22modelIdSelector%22%3A%22appTeNFYcUiYfGcR6%22%2C%22action%22%3A%22readSignedAttachmentUrls%22%7D%5D%2C%22shareId%22%3A%22shro4hnLq63fT8psX%22%2C%22applicationId%22%3A%22appTeNFYcUiYfGcR6%22%2C%22generationNumber%22%3A0%2C%22expires%22%3A%222025-01-16T00%3A00%3A00.000Z%22%2C%22signature%22%3A%22c00120076892e9d47e46bae23cb5fdc387b8a19d723dee6892fa01081a7fa3d7%22%7D", {
@@ -47,20 +59,31 @@ async function fetchData() {
             }); 
         }
 
-        await fs.promises.writeFile("cache.json", JSON.stringify({
+        await client.set("doubloon_lb", JSON.stringify({
             cachedAt: Date.now(),
             data
-        }), "utf-8");
+        }));
 
         return data;
     } else {
         console.error("Failed to fetch data from Airtable, using cache instead.");
 
-        const cache = await fs.promises.readFile("cache.json", "utf-8");
+        const cache = await client.get("doubloon_lb");
 
-        const { data } = JSON.parse(cache);
+        if (cache) {
+            const { data } = JSON.parse(cache);
 
-        return data;
+            return data;
+        } else {
+            console.error("We are so fucking cooked.");
+            console.error("imma try to use silly cache.json, if thats gone we cooked for real");
+
+            const cache = await fs.promises.readFile("cache.json", "utf-8");
+
+            const { data } = JSON.parse(cache);
+
+            return data;
+        }
     }
 }
 
@@ -69,11 +92,24 @@ export async function GET({ url }) {
     const total = url.searchParams.get("total") == "true" || false;
 
     try {
-        const cache = await fs.promises.readFile("cache.json", "utf-8");
+        const cache = await client.get("doubloon_lb");
 
-        const { cachedAt, data } = JSON.parse(cache);
+        if (cache) {
+            let { cachedAt, data } = JSON.parse(cache);
 
-        if (Date.now() - cachedAt > 1000 * 60 * 5) {
+            if (total) {
+                data.sort((a, b) => b.total_doubloons - a.total_doubloons);
+            } else {
+                data.sort((a, b) => b.current_doubloons - a.current_doubloons);
+            }
+
+            return Response.json({
+                users: data.slice(page * 25 - 25, page * 25),
+                pages: Math.ceil(data.length / 25),
+                opted_in: data.length,
+                time_since_last_update: Date.now() - cachedAt
+            });
+        } else {
             let data = await fetchData();
 
             if (total) {
@@ -88,21 +124,10 @@ export async function GET({ url }) {
                 opted_in: data.length,
                 time_since_last_update: 0
             });
-        } else {
-            if (total) {
-                data.sort((a, b) => b.total_doubloons - a.total_doubloons);
-            } else {
-                data.sort((a, b) => b.current_doubloons - a.current_doubloons);
-            }
-
-            return Response.json({
-                users: data.slice(page * 25 - 25, page * 25),
-                pages: Math.ceil(data.length / 25),
-                opted_in: data.length,
-                time_since_last_update: Date.now() - cachedAt
-            });
         }
     } catch (e) {
+        console.error("We cooked: ", e);
+
         let data = await fetchData();
 
         if (total) {
