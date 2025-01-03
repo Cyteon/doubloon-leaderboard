3d6e91e0a8ab63fd12298d0ef6903b6c32a2c391
Cyteon <129582290+Cyteon@users.noreply.github.com>
2025-01-03 23:23:23 +0100

    Fuck me

--- src/routes/api/v1/data/+server.ts
+++ src/routes/api/v1/data/+server.ts
@@ -12,6 +12,8 @@ client.connect()
         console.error("Failed to connect to Redis: ", e);
     });
 
+client.del("doubloon_lb");
+
 async function fetchData() {
     const res = await fetch("https://airtable.com/v0.3/application/appTeNFYcUiYfGcR6/readForSharedPages?stringifiedObjectParams=%7B%22includeDataForPageId%22%3A%22pagblWwXt2nF2bRKu%22%2C%22shouldIncludeSchemaChecksum%22%3Atrue%2C%22expectedPageLayoutSchemaVersion%22%3A26%2C%22shouldPreloadQueries%22%3Atrue%2C%22shouldPreloadAllPossibleContainerElementQueries%22%3Atrue%2C%22urlSearch%22%3A%22iyC4S%253Asort%3DeyJwZWxyRDBBVmZJU282c0RGbCI6eyJjb2x1bW5JZCI6ImZsZDRRb2Z2NUxNR3RsRmxwIiwiYXNjZW5kaW5nIjpmYWxzZX19%22%2C%22includeDataForExpandedRowPageFromQueryContainer%22%3Atrue%2C%22includeDataForAllReferencedExpandedRowPagesInLayout%22%3Atrue%2C%22navigationMode%22%3A%22view%22%7D&requestId=reqF7W1PBvHXwBOfz&accessPolicy=%7B%22allowedActions%22%3A%5B%7B%22modelClassName%22%3A%22page%22%2C%22modelIdSelector%22%3A%22pagblWwXt2nF2bRKu%22%2C%22action%22%3A%22read%22%7D%2C%7B%22modelClassName%22%3A%22application%22%2C%22modelIdSelector%22%3A%22appTeNFYcUiYfGcR6%22%2C%22action%22%3A%22readForSharedPages%22%7D%2C%7B%22modelClassName%22%3A%22application%22%2C%22modelIdSelector%22%3A%22appTeNFYcUiYfGcR6%22%2C%22action%22%3A%22readSignedAttachmentUrls%22%7D%5D%2C%22shareId%22%3A%22shro4hnLq63fT8psX%22%2C%22applicationId%22%3A%22appTeNFYcUiYfGcR6%22%2C%22generationNumber%22%3A0%2C%22expires%22%3A%222025-01-16T00%3A00%3A00.000Z%22%2C%22signature%22%3A%22c00120076892e9d47e46bae23cb5fdc387b8a19d723dee6892fa01081a7fa3d7%22%7D", {
         "credentials": "include",
@@ -62,7 +64,7 @@ async function fetchData() {
         await client.set("doubloon_lb", JSON.stringify({
             cachedAt: Date.now(),
             data
-        }));
+        }), { EX: 300 });
 
         return data;
     } else {
